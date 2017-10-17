b861dde38c114145f6e0287a4ae1aaa58f6f191d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2017-10-17 13:59:47 +0300

    Add store

---
+++ template/frontend/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import state from './state'
+import actions from './actions'
+import mutations from './mutations'
+import getters from './getters'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  actions,
+  mutations,
+  getters
+})
