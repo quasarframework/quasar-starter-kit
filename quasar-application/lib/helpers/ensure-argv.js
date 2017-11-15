cc5faf81966f5e6b6024d41189a6ec78ea9ff62f
Dan Popescu <pdan.popescu@gmail.com>
2017-11-15 15:42:26 +0200

    Show all targets for electron and cordova and limit to list

--- quasar-application/lib/helpers/ensure-argv.js
+++ quasar-application/lib/helpers/ensure-argv.js
@@ -1,15 +1,14 @@
-const error = require('./logger')('app:ensure-argv', 'red')
-const warn = require('./logger')('app:ensure-argv', 'yellow')
+const warn = require('./logger')('app:ensure-argv', 'red')
 
 module.exports = function (argv, command) {
   if (['dev', 'build'].includes(command)) {
     if (!['spa', 'pwa', 'cordova', 'electron'].includes(argv.mode)) {
-      error(`Unknown mode "${ argv.mode }"`)
+      warn(`Unknown mode "${ argv.mode }"`)
       process.exit(1)
     }
 
     if (!['mat', 'ios'].includes(argv.theme)) {
-      error(`Unknown theme "${ argv.theme }"`)
+      warn(`Unknown theme "${ argv.theme }"`)
       process.exit(1)
     }
 
@@ -17,11 +16,12 @@ module.exports = function (argv, command) {
       if (!argv.target) {
         argv.target = 'android'
       }
-      else if (!['android', 'ios'].includes(argv.target)) {
-        warn(`Unknown target "${ argv.target }" for cordova`) // allow unknown new targets
+      else if (!['android', 'ios', 'blackberry10', 'browser', 'osx', 'ubuntu', 'webos', 'windows', 'www'].includes(argv.target)) {
+        warn(`Unknown target "${ argv.target }" for cordova`)
+        process.exit(1)
       }
       if (command === 'dev' && (argv.hostname === undefined || ['localhost', '127.0.0.1', '::1'].includes(argv.hostname.toLowerCase()))) {
-        error('Cordova DEV requires you to specify external IP address or HOSTNAME as -H IP|HOSTNAME"')
+        warn('Cordova DEV requires you to specify external IP address or HOSTNAME as -H IP|HOSTNAME"')
         process.exit(1)
       }
     }
@@ -30,18 +30,19 @@ module.exports = function (argv, command) {
       if (!argv.target) {
         argv.target = 'all'
       }
-      else if (!['all', 'darwin', 'win32', 'linux'].includes(argv.target)) {
-        warn(`Unknown target "${ argv.target }" for electron`) // allow unknown new targets
+      else if (!['all', 'darwin', 'win32', 'linux', 'mas'].includes(argv.target)) {
+        warn(`Unknown target "${ argv.target }" for electron`)
+        process.exit(1)
       }
     }
   }
   else if (command === 'mode') {
     if (argv.add !== undefined && !['pwa', 'cordova', 'electron'].includes(argv.add)) {
-      error(`Unknown mode "${ argv.add }" to add`)
+      warn(`Unknown mode "${ argv.add }" to add`)
       process.exit(1)
     }
     if (argv.remove !== undefined && !['pwa', 'cordova', 'electron'].includes(argv.remove)) {
-      error(`Unknown mode "${ argv.remove }" to remove`)
+      warn(`Unknown mode "${ argv.remove }" to remove`)
       process.exit(1)
     }
   }
