fb9d7ca6978419f277fb6ad26767c5d2778f41fe
BenoitRanque <ranque.benoit@gmail.com>
2017-12-09 15:25:04 -0400

    Add automatic loading of routes in pages
    Feature aimed at beginners.
    Also pushing a commit for default layout with automatic list of all routes

--- template/src/router/routes.js
+++ template/src/router/routes.js
@@ -1,10 +1,31 @@
+function loadpage (path) {
+  return () => import(`pages/${path}`)
+}
+
+const pages = require.context('../pages', true, /^\.\/.*\.vue$/) // Requires all files in pages folder
+  .keys()
+  .filter(page => page.split('/').length >= 2) // remove folders
+  .filter(page => page.indexOf('.vue')) // keep only vue files
+  .map(page => page.slice(2).slice(0, -4)) // remove leading './' and trailing '.vue'
+  .filter(page => {
+    switch (page) {
+      // add exceptions for special pages
+      case 'index':
+      case '404':
+        return false
+      default:
+        return true
+    }
+  })
 
 export default [
   {
     path: '/',
     component: () => import('layouts/default'),
     children: [
-      { path: '', component: () => import('pages/index') },
+      { path: '', component: loadpage('index') },
+      { path: '404', component: loadpage('404') },
+      ...pages.map(p => { return { path: p, component: loadpage(p) } }),
       { path: 'index2/test', component: () => import('pages/index2') },
       { path: 'action-sheet', component: () => import('pages/demo/action-sheet') },
       { path: 'alert', component: () => import('pages/demo/alert') },
@@ -19,6 +40,8 @@ export default [
 
   { // Always leave this as last one
     path: '*',
-    component: () => import('pages/404')
+    redirect: '/404'
   }
 ]
+
+export { pages }
