269d7159fd0a596de9bfdd3f8745183f9f9c72f4
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2018-01-17 15:38:53 +0100

    Update 404.vue
    Polish 404 error page

--- template/src/pages/404.vue
+++ template/src/pages/404.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-center">
-    <div>404</div>
-    <router-link to="/">To index</router-link>
-  </div>
+  <q-page class="flex flex-center text-center">
+    <h1>Nothing here (404)</h1>
+    <q-btn @click="$router.push('/')" color="primary">Go back</q-btn>
+  </q-page>
 </template>
