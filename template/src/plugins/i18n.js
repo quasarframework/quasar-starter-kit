2a5e60f92fb22c234a7568754c4e1c97755ffad9
BenoitRanque <ranque.benoit@gmail.com>
2018-02-23 17:04:40 -0400

    Removed unused variable "store"

--- template/src/plugins/i18n.js
+++ template/src/plugins/i18n.js
@@ -1,7 +1,7 @@
 import VueI18n from 'vue-i18n'
 import messages from 'src/i18n'
 
-export default ({ app, store, Vue }) => {
+export default ({ app, Vue }) => {
   Vue.use(VueI18n)
 
   // Set i18n instance on app
