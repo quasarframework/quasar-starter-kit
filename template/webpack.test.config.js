ec90be8aa9fab9eb0a559a60f7c9d236a106fa3f
NEWGEN\arushi-jain <arushi-jain@newgen.co.in>
2018-05-09 14:29:08 +0530

    reverted webpack changes

--- template/webpack.test.config.js
+++ template/webpack.test.config.js
@@ -1,58 +1,58 @@
-var webpack = require('webpack')
+// var webpack = require('webpack')
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
-}
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.vue$/,
+//         loader: 'vue-loader'
+//       },
+//       {
+//         test: /\.js$/,
+//         loader: 'babel-loader',
+//         exclude: /node_modules/
+//       },
+//       {
+//         test: /\.(png|jpg|gif|svg)$/,
+//         loader: 'file-loader',
+//         options: {
+//           name: '[name].[ext]?[hash]'
+//         }
+//       }
+//     ]
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//     noInfo: true
+//   },
+//   performance: {
+//     hints: false
+//   },
+//   devtool: '#eval-source-map'
+// }
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports.devtool = '#source-map'
+//   module.exports.plugins = (module.exports.plugins || []).concat([
+//     new webpack.DefinePlugin({
+//       'process.env': {
+//         NODE_ENV: '"production"'
+//       }
+//     }),
+//     new webpack.optimize.UglifyJsPlugin({
+//       sourceMap: true,
+//       compress: {
+//         warnings: false
+//       }
+//     }),
+//     new webpack.LoaderOptionsPlugin({
+//       minimize: true
+//     })
+//   ])
+// }
 
-// test specific setups
-if (process.env.NODE_ENV === 'test') {
-  module.exports.externals = [require('webpack-node-externals')()]
-  module.exports.devtool = 'inline-cheap-module-source-map'
-}
+// // test specific setups
+// if (process.env.NODE_ENV === 'test') {
+//   module.exports.externals = [require('webpack-node-externals')()]
+//   module.exports.devtool = 'inline-cheap-module-source-map'
+// }
