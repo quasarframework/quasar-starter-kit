415e9393b5cb7a93c300c8bebc9dbac4e41ab65a
NEWGEN\arushi-jain <arushi-jain@newgen.co.in>
2018-05-17 15:17:00 +0530

    updated path in convert.js

--- template/build/convert.js
+++ template/build/convert.js
@@ -1,3 +1,3 @@
 var shell= require('shelljs')
 
-shell.exec('xcopy /e /v /Y /f src-cordova\\www D:\\Developement\\www')
+shell.exec('xcopy /e /v /Y /f src-cordova\\www D:\demo\eclipse-workspace\MainActivity\assets\www')
