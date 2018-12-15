fc2208073041482af7837eacfbec2f4bb4057352
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2018-12-15 17:12:07 +0200

    feat: Use @quasar/babel-preset-app

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,34 +1,5 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env', {
-        modules: false,
-        loose: false,
-        useBuiltIns: 'usage'
-      }
-    ]
-  ],
-  plugins: [
-    // Stage 2
-    [
-      '@babel/plugin-proposal-decorators', {
-        legacy: true
-      }
-    ],
-    '@babel/plugin-proposal-function-sent',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-throw-expressions',
-
-    // Stage 3
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    [
-      '@babel/plugin-proposal-class-properties', {
-        loose: false
-      }
-    ],
-    '@babel/plugin-proposal-json-strings'
-  ],
-  'comments': false
+    '@quasar/babel-preset-app'
+  ]
 }
