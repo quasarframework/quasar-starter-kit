e1617bb031d6bace76d0c32b9cdc94e491e43ff2
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-02-08 22:49:14 +0200

    fix: Docs link in app

--- template/src/layouts/MyLayout.vue
+++ template/src/layouts/MyLayout.vue
@@ -27,13 +27,13 @@
     >
       <q-list>
         <q-item-label header>Essential Links</q-item-label>
-        <q-item clickable tag="a" target="_blank" href="http://quasar-framework.org">
+        <q-item clickable tag="a" target="_blank" href="http://v1.quasar-framework.org">
           <q-item-section avatar>
             <q-icon name="school" />
           </q-item-section>
           <q-item-section>
             <q-item-label>Docs</q-item-label>
-            <q-item-label caption>quasar-framework.org</q-item-label>
+            <q-item-label caption>v1.quasar-framework.org</q-item-label>
           </q-item-section>
         </q-item>
         <q-item clickable tag="a" target="_blank" href="https://github.com/quasarframework/">
