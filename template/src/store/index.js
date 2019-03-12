d61cee00d5b19c377ef1959259b6a630ef95fa36
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-03-12 23:08:20 +0200

    feat: Enable strict mode for vuex in development #58

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -14,7 +14,11 @@ export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
       // example
-    }
+    },
+
+    // enable strict mode (adds overhead!)
+    // for dev mode only
+    strict: process.env.DEV
   })
 
   return Store
