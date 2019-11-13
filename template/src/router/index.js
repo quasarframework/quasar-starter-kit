69a61d969d20d58bdc425c75e383cec87f96f3f5
ejez <48388358+ejez@users.noreply.github.com>
2019-11-13 13:08:24 +0300

    fix(router): minor typo in 'router/index.js' (#73)

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -15,7 +15,7 @@ export default function (/* { store, ssrContext } */) {
     scrollBehavior: () => ({ x: 0, y: 0 }),
     routes,
 
-    // Leave these as is and change from quasar.conf.js instead!
+    // Leave these as they are and change in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
     mode: process.env.VUE_ROUTER_MODE,
