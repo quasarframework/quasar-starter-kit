4114ea5b9b6e5c2233ea4f7d8aa8235a3c790437
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2019-12-09 19:07:30 +0300

    feat: Added a clarification comment about `build` to quasar.conf.js

--- template/quasar.conf.js
+++ template/quasar.conf.js
@@ -84,6 +84,7 @@ module.exports = function (ctx) {
       showProgress: true,
       gzip: false,
       analyze: false,
+      // Options below are automatically set depending on the env, set them if you want to override
       // preloadChunks: false,
       // extractCSS: false,
 
