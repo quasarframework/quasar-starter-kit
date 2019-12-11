17a72928e107e7c06a21ad984f63517b822fd7c9
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-12-11 13:18:47 +0200

    feat: Update to @quasar/app v1.4 specs in regards to store and router initialization

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -7,7 +7,11 @@ Vue.use(Vuex)
 
 /*
  * If not building with SSR mode, you can
- * directly export the Store instantiation
+ * directly export the Store instantiation;
+ *
+ * The function below can be async too; either use
+ * async/await or return a Promise which resolves
+ * with the Store instance.
  */
 
 export default function (/* { ssrContext } */) {
