4e47c4811b540bf5bfc5fd5fc94941003c6cfb09
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2019-12-12 12:27:20 +0300

    style: Removed a trailing comma in quasar.conf.js template

--- template/quasar.conf.js
+++ template/quasar.conf.js
@@ -164,7 +164,7 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
     cordova: {
-      id: '{{ cordovaId }}',
+      id: '{{ cordovaId }}'
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
