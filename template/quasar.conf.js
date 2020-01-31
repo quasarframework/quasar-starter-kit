06f2ef801ca03cf030734a41a7b4baf97dc8d2af
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-01-31 20:15:55 +0200

    feat: add "line-awesome" to the list

--- template/quasar.conf.js
+++ template/quasar.conf.js
@@ -27,6 +27,7 @@ module.exports = function (ctx) {
       // 'fontawesome-v5',
       // 'eva-icons',
       // 'themify',
+      // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
       'roboto-font', // optional, you are not bound to it
