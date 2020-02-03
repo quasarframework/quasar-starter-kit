0990e9dc4d00c672cbacd71a7b6fe7f6cfbc933e
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-02-03 12:12:21 +0200

    feat: small tweaks to layout

--- template/src/layouts/MainLayout.vue
+++ template/src/layouts/MainLayout.vue
@@ -23,14 +23,14 @@
       v-model="leftDrawerOpen"
       show-if-above
       bordered
-      content-class="bg-grey-2"
+      content-class="bg-grey-1"
     >
       <q-list>
-        <q-item-label header>Essential Links</q-item-label>
+        <q-item-label header class="text-grey-8">Essential Links</q-item-label>
         <EssentialLink
-          v-for="essentialLink in essentialLinks"
-          :key="essentialLink.title"
-          v-bind="essentialLink"
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
         />
       </q-list>
     </q-drawer>
@@ -42,13 +42,15 @@
 </template>
 
 <script>
-import EssentialLink from '../components/EssentialLink'
+import EssentialLink from 'components/EssentialLink'
 
 export default {
   name: 'MainLayout',
+
   components: {
     EssentialLink
   },
+
   data () {
     return {
       leftDrawerOpen: false,
