2a5f455852de8c7dab806579e358752d9ca185aa
Paolo Caleffi <p.caleffi@dreamonkey.com>
2020-02-05 16:50:09 +0100

    fix(i18n): fix configuration error

--- template/src/boot/i18n.ts
+++ template/src/boot/i18n.ts
@@ -1,5 +1,6 @@
 import { boot } from 'quasar';
 import messages from 'src/i18n';
+import Vue from 'vue';
 import VueI18n from 'vue-i18n';
 
 declare module 'vue/types/vue' {
@@ -8,15 +9,15 @@ declare module 'vue/types/vue' {
   }
 }
 
+Vue.use(VueI18n);
+
 export const i18n = new VueI18n({
   locale: "en-us",
   fallbackLocale: "en-us",
   messages
 });
 
-export default boot(({ app, Vue }) => {
-  Vue.use(VueI18n);
-
+export default boot(({ app }) => {
   // Set i18n instance on app
   app.i18n = i18n;
 });
