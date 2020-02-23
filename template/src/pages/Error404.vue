d409f7f50f528e2d45908219c75fe660a742e912
Ejez <ejez@protonmail.ch>
2020-02-23 09:17:17 +0300

    chore(eslint): Keep eslint happy for all vue plugin rules

--- template/src/pages/Error404.vue
+++ template/src/pages/Error404.vue
@@ -6,7 +6,9 @@
         style="width:30vw;max-width:150px;"
       >
     </p>
-    <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
+    <p class="text-faded">
+      Sorry, nothing here...<strong>(404)</strong>
+    </p>
     <q-btn
       color="secondary"
       style="width:200px;"
