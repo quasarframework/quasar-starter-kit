0b5a784210e02a17b225d72d0c3f346d7252abd3
Ejez <48388358+ejez@users.noreply.github.com>
2020-02-23 09:44:07 +0300

    chore(eslint): Keep eslint happy for all vue plugin rules (#92)

--- template/src/components/EssentialLink.vue
+++ template/src/components/EssentialLink.vue
@@ -1,12 +1,22 @@
 <template>
-  <q-item clickable tag="a" target="_blank" :href="link">
-    <q-item-section v-if="icon" avatar>
+  <q-item
+    clickable
+    tag="a"
+    target="_blank"
+    :href="link"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
       <q-icon :name="icon" />
     </q-item-section>
 
     <q-item-section>
       <q-item-label>\{{ title }}</q-item-label>
-      <q-item-label caption>\{{ caption }}</q-item-label>
+      <q-item-label caption>
+        \{{ caption }}
+      </q-item-label>
     </q-item-section>
   </q-item>
 </template>
