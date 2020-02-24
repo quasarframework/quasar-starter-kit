8286dc6da5a02bedf281c6a69bede96d37d3a605
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-02-24 11:31:36 +0200

    feat: small tweaks to quasar.conf.js

--- template/quasar.conf.js
+++ template/quasar.conf.js
@@ -80,11 +80,12 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      scopeHoisting: true,
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      showProgress: true,
-      gzip: false,
-      analyze: false,
+
+      // showProgress: false,
+      // gzip: true,
+      // analyze: true,
+
       // Options below are automatically set depending on the env, set them if you want to override
       // preloadChunks: false,
       // extractCSS: false,
