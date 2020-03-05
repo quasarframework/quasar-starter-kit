8dbef30c325c5ea585f983a02634374e1bcacb53
Paolo Caleffi <p.caleffi@dreamonkey.com>
2020-03-05 12:55:40 +0100

    refactor(lint): update comments

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,18 +1,21 @@
 {{#preset.typescript}}const { resolve } = require('path');
 {{/preset.typescript}}
 module.exports = {
+  // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+  // This option interrupts the configuration hierarchy at this file
+  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
   root: true,
 
   {{#preset.typescript}}
+  // https://eslint.vuejs.org/user-guide/#how-to-use-custom-parser
   // Must use parserOptions instead of "parser" to allow vue-eslint-parser to keep working
-  // See https://eslint.vuejs.org/user-guide/#how-to-use-custom-parser
   // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
   {{/preset.typescript}}
   parserOptions: {
     {{#if preset.typescript}}
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
+    // https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint
     // Needed to make the parser take into account 'vue' files
-    // See https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
-    // See https://github.com/TypeStrong/fork-ts-checker-webpack-plugin#eslint
     extraFileExtensions: ['.vue'],
     parser: '@typescript-eslint/parser',
     project: resolve(__dirname, './tsconfig.json'),
@@ -72,8 +75,8 @@ module.exports = {
     'vue',
 
     {{#if_eq lintConfig "prettier"}}
+    // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
     // Prettier has not been included as plugin to avoid performance impact
-    // see https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
     // add it as an extension for your IDE
     {{/if_eq}}
   ],
