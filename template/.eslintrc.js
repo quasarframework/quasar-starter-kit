6e010399f5018bddcb880bbc8b1104e8a1ba7aaf
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-04-10 14:24:31 +0300

    Update .eslintrc.js

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -48,7 +48,7 @@ module.exports = {
     // Uncomment any of the lines below to choose desired strictness,
     // but leave only one uncommented!
     // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/essential' // Priority A: Essential (Error Prevention)
+    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
     // 'plugin:vue/strongly-recommended' // Priority B: Strongly Recommended (Improving Readability)
     // 'plugin:vue/recommended' // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
