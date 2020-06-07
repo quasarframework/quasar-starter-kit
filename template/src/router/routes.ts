144b4d6399b8444bf800c8166095e83b1c470c16
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-06-07 14:22:10 +0300

    feat: small tweaks to 404 page

--- template/src/router/routes.ts
+++ template/src/router/routes.ts
@@ -4,16 +4,17 @@ const routes: RouteConfig[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/Index.vue') }]
-  }
-];
+    children: [
+      { path: '', component: () => import('pages/Index.vue') }
+    ]
+  },
 
-// Always leave this as last one
-if (process.env.MODE !== 'ssr') {
-  routes.push({
+  // Always leave this as last one,
+  // but you can also remove it
+  {
     path: '*',
     component: () => import('pages/Error404.vue')
-  });
-}
+  }
+];
 
 export default routes;
