b24b1e0710c1f891630958bcbad4310be93be6b6
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-11-23 19:34:35 +0200

    feat: linting upgrade for vue3

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -47,9 +47,9 @@ module.exports = {
     // Uncomment any of the lines below to choose desired strictness,
     // but leave only one uncommented!
     // See https://eslint.vuejs.org/rules/#available-rules
-    'plugin:vue/essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+    'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
+    // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
+    // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
     {{#if_eq lintConfig "standard"}}
     'standard'
