9b4057c0594860b873ef71fada2353e0532ebfa2
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2020-11-25 20:14:30 +0200

    feat: update store to latest specs

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -1,9 +1,6 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 
 // import example from './module-example'
-
-Vue.use(Vuex)
 
 /*
  * If not building with SSR mode, you can
@@ -15,13 +12,13 @@ Vue.use(Vuex)
  */
 
 export default function (/* { ssrContext } */) {
-  const Store = new Vuex.Store({
+  const Store = createStore({
     modules: {
       // example
     },
 
     // enable strict mode (adds overhead!)
-    // for dev mode only
+    // for dev mode and --debug builds only
     strict: process.env.DEBUGGING
   })
 
