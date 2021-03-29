5b7f4ede885fe3c746cb47fc38fa240617f05787
Paolo Caleffi <p.caleffi@dreamonkey.com>
2021-03-29 18:19:37 +0200

    feat(ts): add Vuex typings

--- template/src/store/index.ts
+++ template/src/store/index.ts
@@ -1,5 +1,10 @@
-import { store } from 'quasar/wrappers';
-import { createStore } from 'vuex';
+import { store } from 'quasar/wrappers'
+import { InjectionKey } from 'vue'
+import {
+  createStore,
+  Store as VuexStore,
+  useStore as vuexUseStore,
+} from 'vuex'
 
 // import example from './module-example'
 // import { ExampleStateInterface } from './module-example/state';
@@ -17,8 +22,18 @@ export interface StateInterface {
   // Define your own store structure, using submodules if needed
   // example: ExampleStateInterface;
   // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
-  example: unknown;
+  example: unknown
 }
+
+// provide typings for `this.$store`
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $store: VuexStore<StateInterface>
+  }
+}
+
+// provide typings for `useStore` helper
+export const storeKey: InjectionKey<VuexStore<StateInterface>> = Symbol()
 
 export default store(function (/* { ssrContext } */) {
   const Store = createStore<StateInterface>({
@@ -28,8 +43,12 @@ export default store(function (/* { ssrContext } */) {
 
     // enable strict mode (adds overhead!)
     // for dev mode and --debug builds only
-    strict: !!process.env.DEBUGGING,
-  });
+    strict: !!process.env.DEBUGGING
+  })
 
   return Store;
-});
+})
+
+export function useStore() {
+  return vuexUseStore(storeKey)
+}
