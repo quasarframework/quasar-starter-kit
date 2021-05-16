1033231e3d91aca344fae263bf385532d4149e7f
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-05-16 04:58:07 +0300

    feat: more instructions on SSR usage with axios #154

--- template/src/boot/axios.ts
+++ template/src/boot/axios.ts
@@ -7,6 +7,12 @@ declare module '@vue/runtime-core' {
   }
 }
 
+// Be careful when using SSR for cross-request state pollution
+// due to creating a Singleton instance here;
+// If any client changes this (global) instance, it might be a
+// good idea to move this instance creation inside of the
+// "export default () => {}" function below (which runs individually
+// for each client)
 const api = axios.create({ baseURL: 'https://api.example.com' });
 
 export default boot(({ app }) => {
@@ -21,4 +27,4 @@ export default boot(({ app }) => {
   //       so you can easily perform requests against your app's API
 });
 
-export { axios, api };
+export { api };
