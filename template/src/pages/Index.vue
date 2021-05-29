f5551aceb97ca5b57a3a9138a14f868cfae895f3
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2021-05-29 07:42:15 +0300

    feat: brand update

--- template/src/pages/Index.vue
+++ template/src/pages/Index.vue
@@ -119,7 +119,8 @@ export default Vue.extend({
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
     >
   </q-page>
 </template>
