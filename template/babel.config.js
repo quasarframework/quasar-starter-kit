68899f2bb743ef83b75bfaab76167dc9e576c928
merceyz <merceyz@users.noreply.github.com>
2021-08-03 18:31:50 +0200

    fix: resolve `@quasar/babel-preset-app` from `@quasar/app`

--- template/babel.config.js
+++ template/babel.config.js
@@ -4,7 +4,7 @@ module.exports = api => {
   return {
     presets: [
       [
-        '@quasar/babel-preset-app',
+        require.resolve('@quasar/babel-preset-app', { paths: [require.resolve('@quasar/app/package.json')] }),
         api.caller(caller => caller && caller.target === 'node')
           ? { targets: { node: 'current' } }
           : {}
