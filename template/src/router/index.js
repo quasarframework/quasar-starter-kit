0025cbe00d3cc70caf97f9f42806e3763309b71d
Milos Paunovic <paun992@hotmail.com>
2021-11-05 22:59:54 +0100

    Disabled `func-names` rule in ESLint

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -14,7 +14,6 @@ Vue.use(VueRouter)
  * with the Router instance.
  */
 
-// eslint-disable-next-line func-names
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
     scrollBehavior: () => ({ x: 0, y: 0 }),
