a591d1ec475e1cbd5658f76b27a008a854fddf5a
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2022-02-10 19:03:42 +0200

    feat: update some links (as vuejs 3 is now default/latest)

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -46,7 +46,7 @@ module.exports = {
 
     // Uncomment any of the lines below to choose desired strictness,
     // but leave only one uncommented!
-    // See https://eslint.vuejs.org/rules/#available-rules
+    // See https://eslint.vuejs.org/rules/#available-rules (look for Vuejs 2 ones)
     'plugin:vue/essential', // Priority A: Essential (Error Prevention)
     // 'plugin:vue/strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
     // 'plugin:vue/recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
